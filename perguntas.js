cb43d16fce7b9d69d542d7b17a2468a90ce172ac
Keirreson <keirreson.ferreira@escola.pr.gov.br>
2024-10-24 09:45:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'Programação', 
+    'O que é Python?', 
+    'O Python é uma linguagem de Programação'
+)
+criaCartao(
+    'Geografia', 
+     'Qual a capital da França?', 
+    'A capital da França é Paris'
+ )
+criaCartao(
+    'Programação', 
+    'O que é uma Função?', 
+    'Uma Função é um bloco de codigo que executa alguma tarefa'
+ )
